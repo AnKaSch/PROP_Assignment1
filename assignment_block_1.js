f13d2dd6eed2215228222c41f8b32dd64f729858
AnKaSch <anna.schuback@gmail.com>
2016-11-25 13:19:14 +0100

    Add files via upload

---
+++ assignment_block_1.js
@@ -0,0 +1,12 @@
+var project = "Assignment block 1";
+print(project);
+
+var myObject;
+//målsättning, skapa myObject
+
+var newOb = Object.create();	//Ska lägga till objekt som parameter
+
+create(Object obj) {
+	
+}
+
