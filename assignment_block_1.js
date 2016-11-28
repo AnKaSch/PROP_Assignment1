28904b06d464bc0ec814a446e56f37a83ef469c1
Cecilia Bergman <cili.bergman@gmail.com>
2016-11-28 13:06:15 +0100

    Update assignment_block_1.js
    Lade till prototypeList. Ändrade create.

--- assignment_block_1.js
+++ assignment_block_1.js
@@ -1,10 +1,17 @@
 var project = "Assignment block 1";
 print(project);
 
-var myObject = { //målsättning, skapa myObject
-    create: function (prototypeList) {
-
-    }
+var myObject = {
+	
+	prototypeList: [],
+	
+	create: function(prototypeList) {
+		var newObj = Object.create(this);
+		return newObj;
+	},
+	call: function(funcName, parameters) {
+		return funcName + " " + parameters;
+	}
 }
 
 var newOb = Object.create();	//Ska lägga till objekt som parameter
