20eb03209b685156f6a03924c3875caaa12180b0
AnKaSch <anna.schuback@gmail.com>
2016-11-29 14:31:33 +0100

    Add files via upload

--- assignment_block_1.js
+++ assignment_block_1.js
@@ -1,20 +1,45 @@
 
-
 var myObject = {
 	
-	create: function(prototypeList) {return prototypeList;},
+	listOfPrototypes: [],
+	
+	get: function(){
+		return listOfPrototypes;
+	},
+	
+	create: function(prototypeList) {
+		var newObj;
+		if(prototypeList === null) {
+			listOfPrototypes = undefined;
+			newObj = Object.create(myObject);
+		} else if (prototypeList[0] === null || prototypeList[0] === undefined){
+			console.log("Tom lista!");
+			listOfPrototypes = prototypeList;
+			newObj = Object.create(myObject);
+		} else {
+			listOfPrototypes = prototypeList;
+			newObj = prototypeList;
+		}
+		console.log("Nytt? "+newObj);
+		return newObj;
+	},
+	
 	call: function(funcName, parameters) {return funcName + " " + parameters;}
-}
+};
 
-var objOne = Object.create(myObject);
-var temp = ["Hello"];
-objOne.create(temp);
-objOne.call("World", " LALALA");
+var objZero = myObject.create(null);
+objZero.func = function(arg) { return "Function1 " + arg};
+var objOne = myObject.create([objZero]);
+var objTwo = myObject.create([]);
+objTwo.func = function(arg) {return "Function2 " + arg};
+var objThree = myObject.create([objOne, objTwo]);
+var result = objThree.call("Function ", + ["Test"]);
+console.log(result);
+
+
+
+
+/*  */
+
 
 //Kan behöva ta med ex. newOb.__proto__ = oldProto;
-
-//var newObj = Object.create();
-
-//create(Object obj) {
-	
-//}
