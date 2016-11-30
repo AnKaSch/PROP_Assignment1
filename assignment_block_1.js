0af2053bfa3a1341d3e5b3dd4a2c54a5e2ce1474
AnKaSch <anna.schuback@gmail.com>
2016-11-30 11:08:49 +0100

    Add files via upload
    Har lagt till en kontroll av parameter hos create(prototypeList)

--- assignment_block_1.js
+++ assignment_block_1.js
@@ -1,42 +1,49 @@
+/*Grupp 9: Cecilia Bergman, Anna Schuback*/
 
 var myObject = {
 	
+	id: 10,
+	
 	listOfPrototypes: [],
 	
-	get: function(){
-		return listOfPrototypes;
-	},
-	
 	create: function(prototypeList) {
-		if(prototypeList === null) {
-			console.log("Nullobjekt");
-			listOfPrototypes = undefined;
-			newObj = Object.create(myObject);
-		} else if (prototypeList[0] === null || prototypeList[0] === undefined){
-			listOfPrototypes = prototypeList;
-			console.log("Tom lista-objekt");
-			newObj = prototypeList;//Object.create(myObject);
-		} else {
-			listOfPrototypes = prototypeList;
-			console.log("Nytt/nya objekt");
-			newObj = prototypeList;
-		}
-		console.log("Nytt? "+newObj);
-		return newObj;
+		if(prototypeList instanceof Array || prototypeList === null) {
+			if(prototypeList === null) {
+				listOfPrototypes = undefined;
+				newObj = Object.create(myObject);
+				console.log("Nullobjekt" + newObj);
+			} else if (prototypeList[0] === null || prototypeList[0] === undefined){
+				listOfPrototypes = prototypeList;
+				newObj = Object.create(myObject);
+				console.log("Tom lista-objekt:" + newObj);
+			} else {
+				console.log(prototypeList.length() + " Längd ");
+				listOfPrototypes = prototypeList;
+				console.log("Nytt/nya objekt: " + " " + prototypeList);
+				newObj = prototypeList;
+			}
+			console.log("Nytt? "+newObj);
+			return newObj;
+			}
 	},
 	
 	call: function(funcName, parameters) {return funcName + " " + parameters;}
 };
 
 var objZero = myObject.create(null);
+console.log(objZero.id);
 objZero.func = function(arg) { return "Function1 " + arg};
 var objOne = myObject.create([objZero]);
 var objTwo = myObject.create([]);
 objTwo.func = function(arg) {return "Function2 " + arg};
 var objThree = myObject.create([objOne, objTwo]);
-var result = objThree.call("Function ", + ["Test"]);
+var result = objThree.call("func", + ["Test"]);
 console.log(result);
 
+
+
+
+/*  */
 
 
 //Kan behöva ta med ex. newOb.__proto__ = oldProto;
