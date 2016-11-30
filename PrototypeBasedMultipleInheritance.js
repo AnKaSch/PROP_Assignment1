2492306dd48fbc3ab2468cabeec9a29e24d83ea8
AnKaSch <anna.schuback@gmail.com>
2016-11-30 16:48:34 +0100

    Add files via upload
    Slutgiltig kod för prototypbaserat arv.

---
+++ PrototypeBasedMultipleInheritance.js
@@ -0,0 +1,46 @@
+/*Grupp 9: Cecilia Bergman, Anna Schuback*/
+
+
+var myObject = {
+	
+	listOfPrototypes: [],
+	
+	create: function(prototypeList) {
+		if(prototypeList instanceof Array || prototypeList === null || prototypeList[0] === null || prototypeList[0] === undefined) {			
+			var newObj = Object.create(myObject);
+			if (prototypeList != null) {
+				for(var i = 0; i < prototypeList.length; i++) {
+        				if (this.listOfPrototypes.indexOf(prototypeList[i]) == - 1) {
+							var value = prototypeList[i];
+							value.__proto__ = myObject;
+							this.listOfPrototypes.push(value);
+						}
+					}
+				}
+			}
+		return newObj;
+	},
+	
+	call: function(funcName, parameters) {
+	if (this.hasOwnProperty(funcName)) {
+		return this[funcName].call(this,parameters);
+
+    } else {
+      for (var i = 0; i < this.listOfPrototypes.length; i++) {
+        var currentPrototype = this.listOfPrototypes[i];
+        if (currentPrototype.hasOwnProperty(funcName)) {
+          return currentPrototype[funcName].call(currentPrototype,parameters);
+        }
+      }
+    }
+  }
+};
+	
+var obj0 = myObject.create(null);
+obj0.func = function(arg) { return "func0: " + arg; };
+var obj1 = myObject.create([obj0]);
+var obj2 = myObject.create([]);
+obj2.func = function(arg) { return "func2: " + arg; };
+var obj3 = myObject.create([obj1, obj2]);
+var result = obj3.call("func", ["hello"]);
+console.log(result);
