898ced8883e5cb29a7e4f7b9d32c841d50f1ce6f
Cecilia Bergman <cili.bergman@gmail.com>
2016-11-30 15:45:49 +0100

    Lekt med Annas kod

--- assignment_block_1.js
+++ assignment_block_1.js
@@ -3,17 +3,54 @@ print(project);
 
 var myObject = {
 	
-	listOfProtos: [],
+	listOfPrototypes: [],
 	
 	create: function(prototypeList) {
-		var newObj = Object.create(myObject);
-		return newObj;
+		if(prototypeList instanceof Array || prototypeList === null) {
+			if(prototypeList === null) {
+				var newObj = Object.create(myObject);
+				//console.log("Nullobjekt" + newObj);
+			} else if (prototypeList[0] === null || prototypeList[0] === undefined){
+				//this.listOfPrototypes = prototypeList;
+				var newObj = Object.create(myObject);
+				//console.log("Tom lista-objekt:" + this.newObj);
+			//} else {
+			} else if (prototypeList != null) {
+				for(var i = 0; i < prototypeList.length; i++) {
+        if (this.listOfPrototypes.indexOf(prototypeList[i]) == - 1) {
+          //this.listOfPrototypes.push(prototypeList[i]);
+					var value = prototypeList[i];
+					//console.log(value + " Value");
+					value.__proto__ = myObject;
+					this.listOfPrototypes.push(value);
+					//console.log(i + " index" + " " + this.listOfPrototypes);
+				}
+			}
+				//console.log("Nytt/nya objekt: " + " " + this.listOfPrototypes);
+				//this.newObj = prototypeList;
+			}
+		}
+			//console.log("Nytt? "+this.newObj);
+			return newObj;
+			//}
 	},
+	
 	call: function(funcName, parameters) {
-		return funcName + " " + parameters;
-	}
-}
+	if (this.hasOwnProperty(funcName)) {
+      //return funcName + ' ' + parameters;
+			return this[funcName].apply(this,parameters);
 
+    } else {
+      for (var i = 0; i < this.listOfPrototypes.length; i++) {
+        var currentPrototype = this.listOfPrototypes[i];
+        if (currentPrototype.hasOwnProperty(funcName)) {
+          return currentPrototype[funcName].apply(currentPrototype,parameters);
+        }
+      }
+    }
+  }
+};
+	
 var obj0 = myObject.create(null);
 obj0.func = function(arg) { return "func0: " + arg; };
 var obj1 = myObject.create([obj0]);
@@ -22,6 +59,5 @@ obj2.func = function(arg) { return "func2: " + arg; };
 var obj3 = myObject.create([obj1, obj2]);
 var result = obj3.call("func", ["hello"]);
 console.log(result);
-	
-}
+
 
