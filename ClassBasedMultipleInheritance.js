284cff0b6400084e89990cab95f2c3639fd80a4c
AnKaSch <anna.schuback@gmail.com>
2016-12-01 15:04:46 +0100

    Add files via upload
    create()-metoden fungerar bättre (dock är den ej fullständig än)

--- ClassBasedMultipleInheritance.js
+++ ClassBasedMultipleInheritance.js
@@ -1,23 +1,59 @@
 /*Grupp 9: Cecilia Bergman, Anna Schuback*/
 
+
 function createClass(className, superClassList) {
+	//Object.defineProperty(this, 'new()', {newObject: function() {return (...skapa objekt...)}});, funderar om detta är ett sätt skugga över new-operatorn och skapa en new()-metod
+	var className;
+	var supClassList = [];
+	console.log("Parametrar " + className + " "+ superClassList); //Endast för att skriva ut parametrar, ska tas bort vid inlämning
 	this.className=className;
-	this.superClassList=superClassList;
-}
+	if(superClassList == null || superClassList instanceof Array || superClassList[0] == null || superClassList[0] == undefined) {
+		if(superClassList == null) {
+		  supClassList = null;
+	} else if(superClassList[0] == undefined) {
+		  supClassList = undefined;
+	} else if (superClassList != null || superClassList != undefined) {
+		  for(var index = 0; index < superClassList.length; index++) {
+			  var value = superClassList[index];
+		      console.log("Index " +value + " Längd på listan "+superClassList.length);
+			  supClassList.push(value);
+		  }
+	}
+		 console.log("Ny lista " + supClassList);  /*Endast för att testa att skriva ut den nya listan*/
+		 return className; //+ this.supClassList;???
+	}	
+};
 
-function new() {
-	
-}
+//new = function() { ev ta bort då new är en operator, bör skugga över funktionen på något sätt
+	//ska returnera ett nytt objekt av en viss klass
+//};
 
-function call(funcName, parameters) {
+call = function (funcName, parameters) {
 	
-}
+};
+
 
 var classZero = createClass("Class0 ", null);
-classZero.func = {return "func0 "+ args;};
+classZero.func = function(arg) {return "func0: "+ arg;};
 var classOne = createClass("Class1", [classZero]);
 var classTwo = createClass("Class2",[]);
-classTwo.func = {return "func2 " + args;};
+classTwo.func = function(arg) {return "func2 " + arg;};
 var classThree = createClass("Class3", [classOne, classTwo]);
-var obj3 = classThree.new();
-var result = obj3.call("func", ["Hello"]);
+//var obj3 = classThree.new();
+//var result = obj3.call("func", ["Hello"]);
+
+
+
+
+
+
+
+
+		//if(superClassList != null) {
+		  //for(var index= 0; index < superClassList.length; index++) {
+			   //var value = superClassList[index];
+			   //console.log(value);
+			   //this.supClassList.push(value);
+		 // }
+		//}
+
