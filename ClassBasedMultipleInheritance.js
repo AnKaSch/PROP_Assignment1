87179d3c9d15920ed10deb9bd28e3996cccd767a
AnKaSch <anna.schuback@gmail.com>
2016-12-02 00:15:46 +0100

    Add files via upload
    Testar createClass()-metoden

--- ClassBasedMultipleInheritance.js
+++ ClassBasedMultipleInheritance.js
@@ -2,58 +2,67 @@
 
 
 function createClass(className, superClassList) {
-	//Object.defineProperty(this, 'new()', {newObject: function() {return (...skapa objekt...)}});, funderar om detta är ett sätt skugga över new-operatorn och skapa en new()-metod
-	var className;
-	var supClassList = [];
+	this.cName=className;
+	//cName = className.toString();
+	console.log(cName + " "+superClassList);
+	
+	supClassList = [];
 	console.log("Parametrar " + className + " "+ superClassList); //Endast för att skriva ut parametrar, ska tas bort vid inlämning
-	this.className=className;
-	if(superClassList == null || superClassList instanceof Array || superClassList[0] == null || superClassList[0] == undefined) {
-		if(superClassList == null) {
-		  supClassList = null;
-	} else if(superClassList[0] == undefined) {
-		  supClassList = undefined;
-	} else if (superClassList != null || superClassList != undefined) {
-		  for(var index = 0; index < superClassList.length; index++) {
-			  var value = superClassList[index];
-		      console.log("Index " +value + " Längd på listan "+superClassList.length);
-			  supClassList.push(value);
-		  }
-	}
-		 console.log("Ny lista " + supClassList);  /*Endast för att testa att skriva ut den nya listan*/
-		 return className; //+ this.supClassList;???
-	}	
+		supClassList = [];
+		if(superClassList == null || superClassList instanceof Array || superClassList[0] == null || superClassList[0] == undefined) {
+			if(superClassList == null) {
+				this.supClassList = null;
+			} else if(superClassList[0] == undefined) {
+				this.supClassList = undefined;
+			} else if (superClassList != null || superClassList != undefined) {
+				for(var index = 0; index < superClassList.length; index++) {
+					var value = superClassList[index];
+					this.supClassList.push(value);
+					}
+			}
+	};
+	
+	console.log(supClassList);
+	
+	//Object.defineProperty(this, 'new()', {newObject: function() {return (...skapa objekt...)}});, funderar om detta är ett sätt skugga över new-operatorn och skapa en new()-metod
+	var createdClass = {
+	  cName,
+		o: console.log("ClassName " + cName),
+		superClassList: this,
+		o1: console.log("List "+superClassList)
+
+	};
+
+		  console.log("Ny lista " + supClassList);  /*Endast för att testa att skriva ut den nya listan*/
+		    return createdClass;//className + " " + supClassList;
 };
+
 
 //new = function() { ev ta bort då new är en operator, bör skugga över funktionen på något sätt
 	//ska returnera ett nytt objekt av en viss klass
 //};
 
+
 call = function (funcName, parameters) {
+	if(this.hasOwnProperty(funcName)) {
+		return funcName + " " + parameters;
+	}
 	
 };
 
-
 var classZero = createClass("Class0 ", null);
-classZero.func = function(arg) {return "func0: "+ arg;};
+console.log("Nytt objekt 0 " + classZero.cName); //ENDAST TESTNING
+
+//classZero.func = function(arg) {return "func0: "+ arg;};
 var classOne = createClass("Class1", [classZero]);
-var classTwo = createClass("Class2",[]);
-classTwo.func = function(arg) {return "func2 " + arg;};
-var classThree = createClass("Class3", [classOne, classTwo]);
-//var obj3 = classThree.new();
+console.log("Nytt objekt 1 " + classOne.cName); //ENDAST TESTNING
+
+//var classTwo = createClass("Class2",[]);
+//console.log("Nytt objekt 2 " + classTwo); //ENDAST TESTNING
+
+//classTwo.func = function(arg) {return "func2 " + arg;};
+//var classThree = createClass("Class3", [classOne, classTwo]);
+//console.log("Nytt objekt 3 " + classThree); //ENDAST TESTNING
+
+//var objThree = classThree.className; //Ska egentligen vara classThree.new();
 //var result = obj3.call("func", ["Hello"]);
-
-
-
-
-
-
-
-
-		//if(superClassList != null) {
-		  //for(var index= 0; index < superClassList.length; index++) {
-			   //var value = superClassList[index];
-			   //console.log(value);
-			   //this.supClassList.push(value);
-		 // }
-		//}
-
