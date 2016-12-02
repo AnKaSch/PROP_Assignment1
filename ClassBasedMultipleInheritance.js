f99aeeb5b1f53a0396f8ec7923485a294656b42f
AnKaSch <anna.schuback@gmail.com>
2016-12-02 09:46:00 +0100

    Add files via upload
    Håller på med new()-funktionen, behöver något ändras så är det bara att korrigera innehållet i koden.

--- ClassBasedMultipleInheritance.js
+++ ClassBasedMultipleInheritance.js
@@ -3,37 +3,38 @@
 
 function createClass(className, superClassList) {
 	this.cName=className;
-	//cName = className.toString();
-	console.log(cName + " "+superClassList);
+	listOfSuperClasses = [];
 	
-	supClassList = [];
-	console.log("Parametrar " + className + " "+ superClassList); //Endast för att skriva ut parametrar, ska tas bort vid inlämning
-		supClassList = [];
-		if(superClassList == null || superClassList instanceof Array || superClassList[0] == null || superClassList[0] == undefined) {
-			if(superClassList == null) {
-				this.supClassList = null;
+	if(superClassList == null || superClassList instanceof Array || superClassList[0] == null || superClassList[0] == undefined) {
+		 if(superClassList == null) {
+			   listOfSuperClasses = null;
 			} else if(superClassList[0] == undefined) {
-				this.supClassList = undefined;
+				 listOfSuperClasses = undefined;
 			} else if (superClassList != null || superClassList != undefined) {
-				for(var index = 0; index < superClassList.length; index++) {
-					var value = superClassList[index];
-					this.supClassList.push(value);
+				 for(var index = 0; index < superClassList.length; index++) {
+					 var value = superClassList[index];
+					 listOfSuperClasses.push(value);
 					}
 			}
 	};
-	
-	console.log(supClassList);
-	
+
 	//Object.defineProperty(this, 'new()', {newObject: function() {return (...skapa objekt...)}});, funderar om detta är ett sätt skugga över new-operatorn och skapa en new()-metod
 	var createdClass = {
 	  cName,
-		o: console.log("ClassName " + cName),
-		superClassList: this,
-		o1: console.log("List "+superClassList)
+		listOfSuperClasses,
+		o1: console.log("KLASS "+cName + " List "+listOfSuperClasses), //Testning
+		
+		new: function() {
+			console.log("Testar");
+	  },
+		
+		getList: function() {
+			return listOfSuperClasses;
+		}
 
-	};
+  };
 
-		  console.log("Ny lista " + supClassList);  /*Endast för att testa att skriva ut den nya listan*/
+		  //console.log("Ny lista " + listOfSuperClasses);  /*Endast för att testa att skriva ut den nya listan*/
 		    return createdClass;//className + " " + supClassList;
 };
 
@@ -51,18 +52,18 @@ call = function (funcName, parameters) {
 };
 
 var classZero = createClass("Class0 ", null);
-console.log("Nytt objekt 0 " + classZero.cName); //ENDAST TESTNING
+console.log("Nytt objekt 0 " + classZero.getList()); //ENDAST TESTNING
 
-//classZero.func = function(arg) {return "func0: "+ arg;};
+classZero.func = function(arg) {return "func0: "+ arg;};
 var classOne = createClass("Class1", [classZero]);
 console.log("Nytt objekt 1 " + classOne.cName); //ENDAST TESTNING
 
-//var classTwo = createClass("Class2",[]);
-//console.log("Nytt objekt 2 " + classTwo); //ENDAST TESTNING
+var classTwo = createClass("Class2",[]);
+console.log("Nytt objekt 2 " + classTwo.cName); //ENDAST TESTNING
 
-//classTwo.func = function(arg) {return "func2 " + arg;};
-//var classThree = createClass("Class3", [classOne, classTwo]);
-//console.log("Nytt objekt 3 " + classThree); //ENDAST TESTNING
+classTwo.func = function(arg) {return "func2 " + arg;};
+var classThree = createClass("Class3", [classOne, classTwo]);
+console.log("Nytt objekt 3 " + classThree.listOfSuperClasses); //ENDAST TESTNING
 
-//var objThree = classThree.className; //Ska egentligen vara classThree.new();
+var objThree = classThree.new();
 //var result = obj3.call("func", ["Hello"]);
